2df2bed97e17bf9f19058cd00a5c269136d1553c
tala <talashelleh9@gmail.com>
2025-12-24 19:27:06 +0200

    Update

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,16 +2,26 @@ pipeline {
     agent any
 
     stages {
+
         stage('Checkout') {
             steps {
                 checkout scm
             }
         }
 
-        stage('Deploy') {
+        stage('Stop old containers') {
             steps {
                 sh '''
                 docker compose down || true
+                docker ps -q --filter "publish=8085" | xargs -r docker stop
+                docker ps -aq --filter "publish=8085" | xargs -r docker rm
+                '''
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                sh '''
                 docker compose up -d --build
                 '''
             }
--- app/info.php
+++ app/info.php
@@ -3,23 +3,25 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $conn = new mysqli("db", "appuser", "apppass", "dictionary_app");
-
 if ($conn->connect_error) {
     die("DB connection failed: " . $conn->connect_error);
 }
 
 if (isset($_GET['word'])) {
-    $word = $conn->real_escape_string($_GET['word']);
-    $sql = "SELECT machine_word FROM words WHERE input_word='$word'";
-    $result = $conn->query($sql);
+    $word = trim($_GET['word']);
 
-    if ($result && $result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo $row['machine_word'];
+    $stmt = $conn->prepare("SELECT machine_word FROM words WHERE input_word=?");
+    $stmt->bind_param("s", $word);
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    if ($res && $res->num_rows > 0) {
+        echo $res->fetch_assoc()['machine_word'];
     } else {
         echo "Term not found";
     }
 }
 
 $conn->close();
+
 ?>
