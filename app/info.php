a2a9fdb9a289351aa8a93bce63e90623136357ce
tala <talashelleh9@gmail.com>
2025-12-24 18:21:42 +0200

    fix

--- app/info.php
+++ app/info.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "appuser", "apppass", "dictionary_app");
+$conn = new mysqli("db", "appuser", "apppass", "dictionary_app");
 
 if ($conn->connect_error) {
-    die("DB connection failed");
+    die("DB connection failed: " . $conn->connect_error);
 }
 
 if (isset($_GET['word'])) {
-    $word = strtolower(trim($_GET['word']));
+    $word = $conn->real_escape_string($_GET['word']);
     $sql = "SELECT machine_word FROM words WHERE input_word='$word'";
     $result = $conn->query($sql);
 
@@ -22,3 +22,4 @@ if (isset($_GET['word'])) {
 }
 
 $conn->close();
+?>
